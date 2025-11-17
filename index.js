8c913d4eddc59b57ad2092a21f79423bb65e61ed
Deepak Tiwari <smdeepaktiwari@gmail.com>
2025-11-17 20:45:34 +0530

    dom day 1

---
+++ index.js
@@ -0,0 +1,33 @@
+let h1 = document.querySelector("h1");
+
+// let inc = document.getElementById("inc");
+// let dec = document.getElementById("dec");
+// let res = document.getElementById("res");
+
+// let a = 0;
+// inc.addEventListener("click", () => {
+//   a++;
+//   h1.innerHTML = a;
+// });
+
+// dec.addEventListener("click", () => {
+//   a--;
+//   h1.innerHTML = a;
+// });
+
+// res.addEventListener("click", () => {
+//   a = 0;
+//   h1.innerHTML = a;
+// });
+
+// let r = Math.floor(Math.random() * 255);
+// let b = Math.floor(Math.random() * 255);
+// let g = Math.floor(Math.random() * 255);
+
+// let body = document.querySelector("body");
+
+// body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+
+// console.log(r);
+// console.log(g);
+// console.log(b);
